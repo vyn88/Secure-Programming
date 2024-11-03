9594d63fc806ce99a8d7fabf1bed709cb874a508
Christopher <christopher.kamidin@binus.ac.id>
2024-11-03 21:16:29 +0700

    Update register.php

--- register.php
+++ register.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OJEKKU - Register Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"> <!-- External CSS Library -->
-    <link rel="stylesheet" type="text/css" href="register.css"> <!-- Custom CSS File -->
+    <link rel="stylesheet" type="text/css" href="register.css">
 </head>
 
 <body>
@@ -57,7 +56,7 @@
             }
         }
 
-        // Initialize check for filled inputs
+        // Initialize check for inputs
         checkInput(document.getElementById('username'));
         checkInput(document.getElementById('email'));
     </script>
